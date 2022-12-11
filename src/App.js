b5864dd800b048f311d146d9a4d66b64c01b974a
Abdul Rehman <114314272+Maani5790@users.noreply.github.com>
2022-12-12 02:44:06 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import BulbOff from "./BulbOff";
+import BulbBroken from "./BulbBroken";
+import { useState } from "react";
+
+function App() {
+  const [breakBulb, setBreakBulb] = useState(false);
+  const [bulb, setBulb] = useState(false);
+
+  return (
+    <div>
+      {breakBulb ? <BulbBroken /> : <BulbOff props={bulb} />}
+
+      <button onClick={() => setBulb(!bulb)}>Toggle</button>
+      <button onClick={() => setBreakBulb(!breakBulb)}>
+        {breakBulb ? `Fix the Bulb` : `Break the Bulb`}
+      </button>
+      
+      
+    </div>
+  );
+}
+
+export default App;
